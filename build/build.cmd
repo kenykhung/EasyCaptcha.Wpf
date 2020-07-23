740090186ebd1072884f2e87f07ced3f462e0a36
Ken Hung <isatohon@gmail.com>
2020-07-23 18:20:03 +0800

    add build.cmd and sample.gif

---
+++ build/build.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+set PJ_D_ROOT=%~dp0..
+
+rem External Tools: powershell (from PowerShell)
+set POWERSHELL=powershell
+
+rem Detect powershell
+%POWERSHELL% /? 1>nul 2>&1
+if errorlevel 1 (
+   echo %POWERSHELL%: NOT FOUND
+   exit /b 1
+)
+echo %POWERSHELL%: Found in path
+
+cd "%PJ_D_ROOT%"
+%POWERSHELL% -ExecutionPolicy Unrestricted -File .\build.ps1
